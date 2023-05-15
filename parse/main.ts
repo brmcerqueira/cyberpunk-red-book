b4e90f2f025f94ec182001dcbd2f129b4303aca8
Bruno <brmcerqueira@gmail.com>
2023-05-15 00:14:56 -0300

    start

--- parse/main.ts
+++ parse/main.ts
@@ -5,6 +5,8 @@ const fileReader = await Deno.open(path.join(Deno.cwd(), "/book.txt"));
 
 let page = 0;
 
+let dic: { [i:number]: number } = {};
+
 for await (const line of readLines(fileReader)) {
     const trimLine = line.trim();
     const value = Number(trimLine);
@@ -15,6 +17,35 @@ for await (const line of readLines(fileReader)) {
     && value > page 
     && (value - page) <= 5) {
         page = value;
-        console.log("pagina: " + page);
+        const data = max(dic);
+        console.log("%cpagina: " + page + " | data: " + data.index + " -> " + data.value, "color: red; font-weight: bold");
+        dic = {};
     }
+    else {
+        for (let i = line.length; i >= 0; i--) {
+            const ch = line.charAt(i);
+            const key = i + 1;
+            if (ch == ' ') {
+                if (dic[key] != undefined) {
+                    dic[key] = dic[key] + 1; 
+                }
+                else {
+                    dic[key] = 1; 
+                }      
+            }     
+        }
+
+        console.log(line);
+    }
+}
+
+function max(dic: { [i:number]: number }): { index: number, value: number } {
+    const result: { index: number, value: number } = { index: -1, value: 0 };
+    for (const key in dic) {
+        if (dic[key] >= result.value) {
+            result.index = Number(key);
+            result.value = dic[key];
+        }
+    }
+    return result;
 }
